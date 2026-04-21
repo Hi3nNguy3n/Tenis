7c92367e174387f04129b5b3b20cbddee8dc1421
Nguyen Minh Phu <minhphu25102005@gmail.com>
2026-04-21 22:16:58 +0700

    fix: use REDIS_URL for deployment

--- backend/app/db/redis_client.py
+++ backend/app/db/redis_client.py
@@ -1,13 +1,14 @@
-# backend/app/db/redis_client.py
 import redis
+import os
 from app.core.config import settings
 
 def get_redis():
-    r = redis.Redis(
-        host=settings.REDIS_HOST,
-        port=settings.REDIS_PORT,
-        db=0,
-        decode_responses=True # Rất quan trọng để nhận về string thay vì bytes
+    # Sử dụng link URL từ biến môi trường, nếu không có thì mặc định về localhost để dev
+    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+    
+    r = redis.from_url(
+        redis_url,
+        decode_responses=True # Giữ nguyên cái này để nhận về string
     )
     try:
         yield r
